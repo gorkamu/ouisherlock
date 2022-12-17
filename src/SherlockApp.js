1bef629250669656d313c82f7eb4f3884026df4d
gorka1802 <gorka@influencity.com>
2022-12-17 20:40:21 +0100

    Added GA

--- src/SherlockApp.js
+++ src/SherlockApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { Container } from './components/ui/Container';
-import { gtag, install } from 'ga-gtag';
+import { install } from 'ga-gtag';
 
 export const SherlockApp = () => {
     install('G-H92YSXYWDD'); 
