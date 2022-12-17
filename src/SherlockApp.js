e5fb4ac2ba4eb672956dc164ae8f193a909fc803
gorka1802 <gorka@influencity.com>
2022-12-17 20:29:21 +0100

    Added proxy

--- src/SherlockApp.js
+++ src/SherlockApp.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { Container } from './components/ui/Container';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'G-H92YSXYWDD';
+
+ReactGA.initialize(TRACKING_ID);
 
 export const SherlockApp = () => {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, [])
+
     return (
         <Provider store={ store }>
             <Container />
